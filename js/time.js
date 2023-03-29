ca478e56b7d09f2fc11e200fbe13c40dd43e088d
Liam Schenk <89917505+Liamschenk@users.noreply.github.com>
2023-03-29 20:43:51 +0200

    update

---
+++ js/time.js
@@ -0,0 +1,12 @@
+var spantime = document.getElementById('time');
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  spantime.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+
+setInterval(time, 1000);
