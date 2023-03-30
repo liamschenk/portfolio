34a94ae1f8f2b7efc67f692450a577e8dbd179ba
Liam Schenk <89917505+Liamschenk@users.noreply.github.com>
2023-03-30 17:45:11 +0200

    update

--- js/accordion.js
+++ js/accordion.js
@@ -1,8 +1,8 @@
-var acc = document.getElementsByClassName("accordion");
+var accordion = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
