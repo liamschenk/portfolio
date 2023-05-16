6960d8b11b7b202cd03d67a1f2a937021efd6e38
Liam Schenk <89917505+Liamschenk@users.noreply.github.com>
2023-05-16 20:55:48 +0200

    update
    LIGHT AND DARK MODE🥳
    
    - Changes to Root variables

--- js/script.js
+++ js/script.js
@@ -13,6 +13,29 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+var toggle = document.getElementById("theme-switch");
+
+var storedTheme =
+  localStorage.getItem("theme") ||
+  (window.matchMedia("(prefers-color-scheme: light)").matches
+    ? "light"
+    : "dark");
+if (storedTheme)
+  document.documentElement.setAttribute("data-theme", storedTheme);
+
+toggle.onclick = function () {
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  var targetTheme = "light";
+
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  }
+
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+};
+
+
 function time() {
   var d = new Date();
   var s = d.getSeconds();
