fcb90d1fabecf364d02e258c9b6effd9f96aab25
Liam Schenk <89917505+liamschenk@users.noreply.github.com>
2023-10-03 15:56:22 +0200

    Portfolio overhaul
    - My Portfolio now only Displays work
    - My entire CV is now readable on read.cv

--- js/script.js
+++ js/script.js
@@ -1,38 +1,11 @@
-var acc = document.getElementsByClassName("accordion-button");
 var spantime = document.getElementById("time");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    /*Toggle between adding and removing the "active" class*/
-    this.classList.toggle("active");
-
-    /*Toggle between hiding and showing the active content*/
-    var content = this.nextElementSibling;
-    content.classList.toggle("animated");
-  });
-}
-
-const toggle = document.querySelector(".theme-switch");
 var storedTheme =
-  localStorage.getItem("theme") ||
   (window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light");
 if (storedTheme)
   document.documentElement.setAttribute("data-theme", storedTheme);
-
-toggle.onclick = function () {
-  var currentTheme = document.documentElement.getAttribute("data-theme");
-  var targetTheme = "light";
-
-  if (currentTheme === "light") {
-    targetTheme = "dark";
-  }
-
-  document.documentElement.setAttribute("data-theme", targetTheme);
-  localStorage.setItem("theme", targetTheme);
-};
 
 function time() {
   var d = new Date();
