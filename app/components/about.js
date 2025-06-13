ba64757d12b843d7d28be135ed133c1c24b0bf31
Liam Schenk <89917505+liamschenk@users.noreply.github.com>
2025-06-13 16:44:05 +0200

    V4.2.4 - Content
    Added a project to the index and arrows to links on about view.

--- app/components/about.js
+++ app/components/about.js
@@ -75,6 +75,15 @@ export default function About({ basics, work, education }) {
                   >
                     {profile.username}
                   </a>
+                  <span>
+                    {" "}
+                    <svg viewBox="-10 0 1598 2048" className={styles.arrow}>
+                      <path
+                        fill="currentColor"
+                        d="M1338 1442h-166v-298q0 -54 3 -116.5t8 -126.5t11 -123.5t13 -107.5l11 42q-23 35 -47.5 70t-50.5 67t-56 62l-725 726l-118 -118l726 -725q30 -30 62.5 -56t67 -50.5t69.5 -47.5l42 11q-64 9 -147 17t-169 13t-158 5h-298v-166h922v922z"
+                      />
+                    </svg>
+                  </span>
                 </p>
               </div>
             ))}
