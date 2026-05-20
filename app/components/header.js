3839d18be337139ef394cf5f15e461331a2f6628
Liam Schenk <89917505+liamschenk@users.noreply.github.com>
2026-05-20 18:41:37 +0200

    V5.6.5 - Logo & Animations

--- app/components/header.js
+++ app/components/header.js
@@ -23,14 +23,23 @@ export default function Header({ route, setRoute }) {
 
           <li>
             <a
-              className="color-secondary no-underline underline-hover"
+              className="color-secondary"
               href="#"
               onClick={(e) => {
                 e.preventDefault();
                 setRoute("index");
               }}
             >
-              Liam Schenk
+              <svg
+                className={styles["header-icon"]}
+                viewBox="0 0 512 512"
+                aria-hidden="true"
+              >
+                <path
+                  fill="currentColor"
+                  d="M279.847 106.925L412.628 327.013C418.157 336.211 418.388 347.603 413.024 357.015C407.661 366.428 397.626 372.4 386.667 372.4H121.103C110.144 372.4 100.109 366.428 94.746 357.015C89.382 347.603 89.613 336.211 95.142 327.013L227.923 106.925C233.399 97.823 243.272 92.4 253.885 92.4C264.498 92.4 274.371 97.823 279.847 106.925Z"
+                />
+              </svg>
             </a>
           </li>
 
--- app/pages/about.js
+++ app/pages/about.js
@@ -85,7 +85,7 @@ export default function About({ basics, work, education, profiles }) {
                 <p className="color-tertiary">{item.network}</p>
                 <p className="color-secondary">
                   <a
-                    className="underline no-underline-hover"
+                    className={`${styles["contact-link"]} underline no-underline-hover`}
                     href={item.link}
                     target={isExternal ? "_blank" : undefined}
                     rel={isExternal ? "noopener noreferrer" : undefined}
@@ -97,18 +97,16 @@ export default function About({ basics, work, education, profiles }) {
                   >
                     {item.username}
                   </a>
-                  {isExternal && (
-                    <svg
-                      className={`${styles["external-icon"]} color-secondary`}
-                      viewBox="-10 0 1598 2048"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M1338 1442h-166v-298q0 -54 3 -116.5t8 -126.5t11 -123.5t13 -107.5l11 42q-23 35 -47.5 70t-50.5 67t-56 62l-725 726l-118 -118l726 -725q30 -30 62.5 -56t67 -50.5t69.5 -47.5l42 11q-64 9 -147 17t-169 13t-158 5h-298v-166h922v922z"
-                      />
-                    </svg>
-                  )}
+                  <svg
+                    className={styles["contact-icon"]}
+                    viewBox="-10 0 1598 2048"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M1338 1442h-166v-298q0 -54 3 -116.5t8 -126.5t11 -123.5t13 -107.5l11 42q-23 35 -47.5 70t-50.5 67t-56 62l-725 726l-118 -118l726 -725q30 -30 62.5 -56t67 -50.5t69.5 -47.5l42 11q-64 9 -147 17t-169 13t-158 5h-298v-166h922v922z"
+                    />
+                  </svg>
                 </p>
               </div>
             );
